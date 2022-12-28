04a6529d4a591d0c842134a9b7ad0302d484f296
王贰拾 <wangwenkai5052@outlook.com>
2022-12-28 23:40:52 +0800

    feat: 20221228

--- 每日一题/2022/12月/2022-12-28/删除字符串两端相同字符后的最短长度.js
+++ 每日一题/2022/12月/2022-12-28/删除字符串两端相同字符后的最短长度.js
@@ -3,7 +3,24 @@
  * @return {number}
  * https://leetcode.cn/problems/minimum-length-of-string-after-deleting-similar-ends/
  */
-var minimumLength = function (s) {};
+var minimumLength = function (s) {
+  let i = 0;
+  let j = s.length - 1;
+  while (s[i] === s[j] && i < j) {
+    if (j - i === 1) {
+      return 0;
+    }
+    if (s[i + 1] == s[i]) {
+      i++;
+    } else if (s[j - 1] == s[j]) {
+      j--;
+    } else {
+      i++;
+      j--;
+    }
+  }
+  return j - i + 1; // 从0开始
+};
 
-const s = "aaabb";
+const s = "cabaabac";
 console.log(minimumLength(s));
