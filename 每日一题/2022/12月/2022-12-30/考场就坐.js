7f80ca7bae39fe03dfd29ebd491b3a8483636951
王贰拾 <wangwenkai5052@outlook.com>
2022-12-30 00:17:31 +0800

    每日一题：2022-12-30

---
+++ 每日一题/2022/12月/2022-12-30/考场就坐.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * https://leetcode.cn/problems/exam-room/
+ */
+var ExamRoom = function (n) {};
+
+/**
+ * @return {number}
+ */
+ExamRoom.prototype.seat = function () {};
+
+/**
+ * @param {number} p
+ * @return {void}
+ */
+ExamRoom.prototype.leave = function (p) {};
+
+/**
+ * Your ExamRoom object will be instantiated and called as such:
+ * var obj = new ExamRoom(n)
+ * var param_1 = obj.seat()
+ * obj.leave(p)
+ */
