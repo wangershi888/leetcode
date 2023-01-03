11263e0477315e9f86b8890b4974f9fe445ef216
王贰拾 <wangwenkai5052@outlook.com>
2023-01-04 00:12:50 +0800

    每日一题： 20230104

---
+++ 每日一题/2023/20230104/有界数组中指定下标处的最大值.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n
+ * @param {number} index
+ * @param {number} maxSum
+ * @return {number}
+ * https://leetcode.cn/problems/maximum-value-at-a-given-index-in-a-bounded-array/
+ */
+var maxValue = function (n, index, maxSum) {};
